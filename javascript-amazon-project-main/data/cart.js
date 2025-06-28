2e7a648484ce563e9a2a24eacf1b25ecbcefab07
Prathamesh <prathameshbhalerao200@gmail.com>
2025-06-28 22:54:49 +0530

    added logic for delivery options

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,11 +1,13 @@
 export let cart=JSON.parse(localStorage.getItem('cart')) || [
   {
     productId:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity:1
+    quantity:1,
+    deliveryOptionId:'1'
   },
   {
     productId:"15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity:2
+    quantity:2,
+    deliveryOptionId:'2'
   }
 
 ]
@@ -28,6 +30,7 @@ export function addtoCart(productId){
                     cart.push({
                 productId:productId,
                 quantity:1
+                ,deliveryOptionId:'1'
             })
              }
              saveTostorage()
@@ -44,4 +47,3 @@ export function removeFromcart(productId){
     cart=newcart
     saveTostorage()
 }
-console.log(cart)
